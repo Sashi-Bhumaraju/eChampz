29b289a2fa68ef1877a6c8c667751aee2a3513d1
SASHiBHUMARAJU2 <sasi.654forever@gmail.com>
2023-05-18 16:51:12 +0530

    updated

--- src/components/Tournaments.js
+++ src/components/Tournaments.js
@@ -1,5 +1,5 @@
 function Tournaments () {
-    return ( <div className="tournaments"> <h1>Tournaments</h1> </div> );
+    return ( <div className="tournaments" style={{display:"flex", justifyContent:"", alignItems:"end", height:"100%", margin:"0px", padding:"0px", backgroundColor:"red"}}> <h1>Tournaments</h1> </div> );
 }
 
 export default Tournaments;
