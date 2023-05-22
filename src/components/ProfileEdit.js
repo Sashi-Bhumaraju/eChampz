eecdb5afb61339296b7ea92a43e4f3f17ffb08a1
SASHiBHUMARAJU2 <sasi.654forever@gmail.com>
2023-05-22 18:33:58 +0530

    'updated'

--- src/components/ProfileEdit.js
+++ src/components/ProfileEdit.js
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import UseInputState from '../hooks/UseInputState';
 import UseSelect from '../hooks/UseSelect';
+import { MdOutlineCancel } from 'react-icons/md'
 function ProfileEdit () {
 
     const user = useSelector((state)=>{
@@ -20,17 +21,28 @@ function ProfileEdit () {
         <div className="profile-edit">
             <div className="profile-edit-body">
                 <form>
+                    <div className="input-box-photo">
+                            <div className='label-photo'>
+                                 <img className='img'></img>
+                            </div>
+                            
+                            <div className='input-box-body'>
+                                <div className='username'>{user.name}</div>
+                                <input className='edit-pic' style={{display:"none"}} id='profile-pic' name='profile-pic' type='file' accept="image/*"></input>
+                                <label for="profile-pic">choose a photo</label>
+                            </div>
+                    </div>
                     <div className='input-box'>
                         <div className="label" >Email </div>
                         <input type="email" value={email} onChange={setEmail}  ></input> 
                     </div>
                     <div className='input-box'>
                         <div className="label" >Phone </div>
-                        <input type="email" value={phone} onChange={setPhone}  ></input> 
+                        <input type="text" value={phone} onChange={setPhone}  ></input> 
                     </div>
                     <div className='input-box'>
                         <div className="label" >User name </div>
-                        <input type="email" value={username} onChange={setUsername}  ></input> 
+                        <input type="text" value={username} onChange={setUsername}  ></input> 
                     </div>
                     <div className='input-box'>
                         <div className="label" >Bio </div>
@@ -45,7 +57,10 @@ function ProfileEdit () {
                         <div className="label" >Date of birth </div>
                         <input type="datetime-local" onChange={(event)=>{setSelectedDate(event); }} value={selectedDate} min="1980-01-01" max={new Date().toISOString().substring(0,10)} ></input>
                     </div>
-                 
+                  <div className='submit'>submit</div>
+                  <div className='profile-edit-cancel'>
+                        <MdOutlineCancel></MdOutlineCancel>
+                  </div>
                 </form>
             </div>
         </div>
